ac53ea423410046fee16de1cc7e963d37ca9df84
Paul Duca <paul.r.duca@gmail.com>
2016-11-10 16:16:17 -0500

    styling conflicts

--- app/components/chat.js
+++ app/components/chat.js
@@ -8,6 +8,7 @@ import {
   TouchableOpacity,
   TouchableHighlight,
   Dimensions,
+  Modal,
 } from 'react-native';
 
 import KeyboardSpacer from 'react-native-keyboard-spacer';
@@ -96,7 +97,7 @@ export default class Chat extends Component {
   handleKey(e) {
     if (e.nativeEvent.key === " ") {
       this.setState({fullWord: true})
-    } 
+    }
     else if (e.nativeEvent.keyCode === 13) {
       console.log("this is an enter press")
     }
@@ -140,8 +141,9 @@ export default class Chat extends Component {
    const data = this.state.dataSource.cloneWithRows(this.state.messageList || [])
 
    return (
-     <View style={styles.container}>
+     <View style={styles.outerContainer}>
         <TouchableHighlight
+
         underlayColor={'#4e4273'}
         onPress={this.onBackPress.bind(this)}
         style={{marginLeft: 15}}
@@ -149,15 +151,15 @@ export default class Chat extends Component {
         <Text style={{color: "#000", padding: 20}}>&lt; Back</Text>
         </TouchableHighlight>
         <Text>Chat</Text>
-        <ListView
-        enableEmptySections
-        dataSource={data}
-        renderRow={(rowData, sectionID, rowID) => (
-          <Text>{rowData}</Text>
+          <ListView
+            enableEmptySections
+            dataSource={data}
+            renderRow={(rowData, sectionID, rowID) => (
+            <Text>{rowData}</Text>
         )}
         />
        <TextInput
-         style={{flex: 2, borderWidth: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#F5FCFF' }}
+         style={styles.textInput}
          value={this.state.message}
          onKeyPress={this.handleKey.bind(this)}
          onChangeText={this.handleChange.bind(this)}
@@ -166,8 +168,8 @@ export default class Chat extends Component {
         {Math.floor(this.state.percentage * 100)}%
        </Text>
        <TouchableHighlight
-        underlayColor={'#4e4273'}
-        onPress={() => this.onSendPress()}
+         underlayColor={'#4e4273'}
+         onPress={() => this.onSendPress()}
         >
         <Text style={styles.sendLabel}>send</Text>
        </TouchableHighlight>
@@ -183,20 +185,27 @@ export default class Chat extends Component {
 }
 
 const styles = StyleSheet.create({
+  outerContainer: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    paddingHorizontal: 20,
+    paddingTop: 20,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  textInput: {
+    borderRadius: 5,
+    borderWidth: 1,
+    height: 44,
+    paddingHorizontal: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  segment: {
+    marginBottom: 10,
   },
+  closeButton: {
+    position: 'absolute',
+    top: 30,
+    left: 10,
+  }
 });
